11a8706ade0e310e8d90d692172817eb5f83605f
Екатерина Лукинова <katyushta@rambler.ru>
2020-12-15 13:35:28 +0300

    form

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+
+$( document ).ready(function() {
+    $('.contact-form').on('submit', function (e) {
+
+        e.preventDefault();
+
+        $.ajax({
+            type: 'post',
+            url: 'form_processing.php',
+            data: $('form').serialize(),
+            success: function () {
+                $('.form-button').addClass('active').text('Thank you')
+            }
+        });
+    });
+});
